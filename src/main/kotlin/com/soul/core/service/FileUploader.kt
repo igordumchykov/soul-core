14003fed9e2c0adef838843e868111dddf74c20e
Igor Dumchykov <Igor_Dumchykov@neimanmarcus.com>
2022-11-07 17:50:10 +0200

    Configure docker file, implement AWS S3 file upload

--- src/main/kotlin/com/soul/core/service/FileUploader.kt
+++ src/main/kotlin/com/soul/core/service/FileUploader.kt
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
-import java.io.File
 import javax.annotation.PostConstruct
 
 @Service
